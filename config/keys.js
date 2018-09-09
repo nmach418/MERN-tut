8a85d2eb243a4e855b715b63e63ecda5a47806ef
Boku <Boku@NHs-MacBook-Pro.local>
2018-09-09 09:31:59 -0400

    Added comment to keys.js file to make it clear that it is a bad idea to hard code usernames and passwords into the file. Also, created file structure for routes, api, items.js. Modified server.js accordingly.

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,5 @@
+// NOTE the hard coded username and password below. NOT a good security practice.
+
 module.exports = {
     mongoURI: 'mongodb://niru:niru123@ds251002.mlab.com:51002/mern_shopping'
 };
