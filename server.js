eeb2f4fd0bc8595b551acabc5e5762b815ec7dad
Boku <Boku@NHs-MacBook-Pro.local>
2018-09-08 23:43:44 -0400

    Assigned env and ports for server to run on in server.js file. Server is up and running on port 5000.

--- server.js
+++ server.js
@@ -7,3 +7,16 @@ const app = express();
 // Bodyparser Middleware
 app.use(bodyParser.json());
 
+// DB config
+const db = require('./config/keys').mongoURI;
+
+// Connect to Mongo
+mongoose.connect(db)
+    .then(() => console.log('MongoDB Connected...'))
+    .catch(err => console.log(err));
+
+
+    const port = process.env.PORT || 5000;
+    
+    app.listen(port, () => console.log(`Server started on ${port}`));
+    
